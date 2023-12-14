77780052bd6913c173a8512233f14efd927d047b
Linus Fridberg <linus@fridbergs.com>
2023-12-14 01:32:21 +0100

    testing

--- scripts/electricity.js
+++ scripts/electricity.js
@@ -10,14 +10,14 @@ async function getData() {
     }
 
     const data = await response.json();
-    console.log(data[3].SEK_per_kWh);
-    const latestPrice = data[3].SEK_per_kWh;
 
+    console.log(data[3].SEK_per_kWh);
     //Spara bara de senaste datan
-    // const latestPrice = data.slice(-1)[0];
+    const latestPrice = data.slice(-1)[0];
     // Handle the array of objects from JSON
     elMarknadSverige(latestPrice);
     console.log(latestPrice);
+    console.log(latestPrice.SEK_per_kWh);
   } catch (error) {
     console.error("Error fetching or parsing data:", error);
   }
