305a65e2d0197aad035b25e8ef6cbdec0b9fb9f7
Linus Fridberg <linus@fridbergs.com>
2023-12-22 09:58:15 +0100

    small changes

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,23 @@
 //Gör Body tillgänglig globalt
 let body;
 
-function generateBackgroundImg(unsplashAccessKey) {
+async function generateBackgroundImg(unsplashAccessKey) {
+  try {
   // Variabel som genererar en bakgrundsbild
   const imageSrc = `https://api.unsplash.com/photos/random?client_id=${unsplashAccessKey}`;
+  const image = await fetch (imageSrc);
+  //kolla upp image.blob - lägg den på -lösningen är nog där. (json blob)
+  console.log(imageSrc);
+  const blobSrc = blob(image)
+  //- > in i rad 14
+  //Appenda blob med imge
+
+
   //Källan till bakgrundsbilderna
-  body.style.backgroundImage = `url("${imageSrc}")`;
+  body.style.backgroundImage = `url("${imageBlob}")`;
 
   console.log("knappen har klickats - Linus ");
+}
 }
 
 async function getApiKey() {
@@ -20,7 +30,7 @@ async function getApiKey() {
       //Konverterar datan i variables.json till json
       const variables = await response.json();
       const unsplashAccessKey = variables["allApis"].accessKey;
-      console.log(unsplashAccessKey);
+
       // Anropa generateBackgroundImg med den hämtade API-nyckeln
       generateBackgroundImg(unsplashAccessKey);
     } else {
@@ -35,8 +45,8 @@ async function getApiKey() {
 
 document.addEventListener("DOMContentLoaded", function () {
   //Kontakt med body
-  body = document.body;
-  const backgroundBtn = document.getElementById("generateBackground");
-
-  backgroundBtn.addEventListener("click", getApiKey);
 });
+body = document.body;
+const backgroundBtn = document.getElementById("generateBackground");
+
+backgroundBtn.addEventListener("click", getApiKey);
--- scripts/electricity.js
+++ scripts/electricity.js
@@ -1,23 +1,47 @@
-const apiUrl =
-  "https://www.elprisetjustnu.se/api/v1/prices/2023/12-13_SE4.json";
+const apiUrlSe4 = `https://www.elprisetjustnu.se/api/v1/prices/${todayYear}/${todayMonth}-${todayDate}_SE4.json`;
+const apiUrlSe3 = `https://www.elprisetjustnu.se/api/v1/prices/${todayYear}/${todayMonth}-${todayDate}_SE3.json`;
+const apiUrlSe2 = `https://www.elprisetjustnu.se/api/v1/prices/${todayYear}/${todayMonth}-${todayDate}_SE2.json`;
+const apiUrlSe1 = `https://www.elprisetjustnu.se/api/v1/prices/${todayYear}/${todayMonth}-${todayDate}_SE1.json`;
+
+const todayDate = new Date();
+const todayMonth = new Date();
+const todayYear = new Date();
+//Aldors kod
+const today = new Date();
+const year = today.getFullYear();
+console.log(year);
 
 async function getData() {
   try {
-    const response = await fetch(apiUrl);
+    const responseSe4 = await fetch(apiUrlSe4);
+    const responseSe3 = await fetch(apiUrlSe3);
+    const responseSe2 = await fetch(apiUrlSe2);
+    const responseSe1 = await fetch(apiUrlSe1);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    if (
+      !responseSe1.ok ||
+      !responseSe2.ok ||
+      !responseSe3.ok ||
+      !responseSe4.ok
+    ) {
+      throw new Error(`HTTP error! Status: ${response.status} `);
     }
 
-    const data = await response.json();
+    const dataSe4 = await responseSe4.json();
 
-    console.log(data[3].SEK_per_kWh);
+    //
+    const dataSe3 = await responseSe3.json();
+
+    //
+    const dataSe2 = await responseSe2.json();
+
+    //
+    const dataSe1 = await responseSe1.json();
+
     //Spara bara de senaste datan
     const latestPrice = data.slice(-1)[0];
     // Handle the array of objects from JSON
     elMarknadSverige(latestPrice);
-    console.log(latestPrice);
-    console.log(latestPrice.SEK_per_kWh);
   } catch (error) {
     console.error("Error fetching or parsing data:", error);
   }
