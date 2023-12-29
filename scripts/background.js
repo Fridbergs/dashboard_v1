918da4094523292532c7a9187032f562c8c0751a
Linus Fridberg <linus@fridbergs.com>
2023-12-29 13:14:05 +0100

    bakgrunden är nu fixad

--- scripts/background.js
+++ scripts/background.js
@@ -1,36 +1,53 @@
-const unsplashAccessKey = "7Ov5vewtA_HqGfVs-hDfvY_hTBwu6X-nIB5kcmgF5IA";
-const queryWord = "barcelona";
+// scripts/background.js
 
-const imageSrc = `https://api.unsplash.com/search/photos?query=${queryWord}&client_id=${unsplashAccessKey}`;
+document.addEventListener("DOMContentLoaded", () => {
+  const unsplashAccessKey = "7Ov5vewtA_HqGfVs-hDfvY_hTBwu6X-nIB5kcmgF5IA";
+  const queryWord = "london";
 
-const getImagesButton = document.querySelector("body");
-const imageContainer = document.body;
+  const imageSrc = `https://api.unsplash.com/search/photos?query=${queryWord}&client_id=${unsplashAccessKey}`;
 
-getImagesButton.addEventListener("click", async () => {
-  try {
-    let randomImage = await getNewImage();
-    setBodyBackground(randomImage);
-  } catch (error) {
-    console.error("Error fetching image:", error);
+  const getImagesButton = document.querySelector(".getImagesButton");
+
+  getImagesButton.addEventListener("click", async () => {
+    try {
+      let randomImage = await getNewImage();
+      setBodyBackground(randomImage);
+      saveBackgroundImage(randomImage);
+    } catch (error) {
+      console.error("Error fetching image:", error);
+    }
+  });
+
+  // On page load, retrieve the background image from localStorage and set it
+  const savedBackground = getSavedBackgroundImage();
+  if (savedBackground) {
+    setBodyBackground(savedBackground);
+  }
+
+  async function getNewImage() {
+    try {
+      let randomNumber = Math.floor(Math.random() * 10);
+      const response = await fetch(imageSrc);
+      const data = await response.json();
+      let allImages = data.results[randomNumber];
+      return allImages.urls.regular;
+    } catch (error) {
+      throw new Error("Failed to fetch image");
+    }
+  }
+
+  function setBodyBackground(imageUrl) {
+    document.body.style.backgroundImage = `url('${imageUrl}')`;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+  }
+
+  function saveBackgroundImage(imageUrl) {
+    localStorage.setItem("backgroundImage", imageUrl);
+  }
+
+  function getSavedBackgroundImage() {
+    return localStorage.getItem("backgroundImage");
   }
 });
-
-async function getNewImage() {
-  try {
-    let randomNumber = Math.floor(Math.random() * 10);
-    const response = await fetch(imageSrc);
-    const data = await response.json();
-    let allImages = data.results[randomNumber];
-    return allImages.urls.regular;
-  } catch (error) {
-    throw new Error("Failed to fetch image");
-  }
-}
-
-function setBodyBackground(imageUrl) {
-  // Set the background image of the body
-  document.body.style.backgroundImage = `url('${imageUrl}')`;
-  document.body.style.backgroundSize = "cover";
-  document.body.style.backgroundPosition = "center";
-  document.body.style.backgroundRepeat = "no-repeat";
-}
